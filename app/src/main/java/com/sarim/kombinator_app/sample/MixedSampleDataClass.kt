ef3c3ad79b85397eba196d9cd5fc8ccaeec07f42
Muhammad Sarim Mehdi <sarim.mehdi.550@gmail.com>
2025-07-13 03:17:43 +0200

    added parameter-level annotations

--- app/src/main/java/com/sarim/kombinator_app/sample/MixedSampleDataClass.kt
+++ app/src/main/java/com/sarim/kombinator_app/sample/MixedSampleDataClass.kt
@@ -2,65 +2,56 @@ package com.sarim.kombinator_app.sample
 
 import com.sarim.kombinator.Kombine
 
-//@Kombine(
-//    allPossibleStringParams = ["abc", "cde"],
-//    allPossibleIntParams = [1],
-//    allPossibleFloatParams = [1f, 2f, 3f],
-//    allPossibleDoubleParams = [1.0, 2.0],
-//    allPossibleLongParams = [1, 2],
-//    allPossibleByteParams = [1, 2],
-//    allPossibleCharParams = ['1', '2', '3'],
-//    allPossibleShortParams = [1, 2],
-//    allPossibleUByteParams = [1u, 2u],
-//    allPossibleUShortParams = [1u, 2u],
-//    allPossibleUIntParams = [1u, 2u],
-//    allPossibleULongParams = [1u, 2u, 45u],
-//)
-//data class MixedSampleDataClass(
-//    val property1: Boolean,
-//    val property2: Boolean,
-//    val property3: String,
-//    val property4: Double,
-//    val property5: UInt,
-//    val property6: UShort,
-//    val property7: UShort,
-//    val property8: Boolean,
-//    val property9: Int,
-//    val property10: Char,
-//)
-//
-//@Kombine(
-//    allPossibleStringParams = ["abc", "cde"],
-//    allPossibleIntParams = [1],
-//    allPossibleFloatParams = [1f, 2f, 3f],
-//    allPossibleDoubleParams = [1.0, 2.0],
-//    allPossibleLongParams = [1, 2],
-//    allPossibleByteParams = [1, 2],
-//    allPossibleCharParams = ['1', '2', '3'],
-//    allPossibleShortParams = [1, 2],
-//    allPossibleUByteParams = [1u, 2u],
-//    allPossibleUShortParams = [1u, 2u],
-//    allPossibleUIntParams = [1u, 2u],
-//    allPossibleULongParams = [1u, 2u, 45u],
-//)
-//data class MixedSampleDataClassSomeParamsAnnotated(
-//    val property1: Boolean,
-//    @Kombine val property2: Boolean,
-//    val property3: String,
-//    @Kombine(allPossibleDoubleParams = [5.0, 110.0]) val property4: Double,
-//    val property5: UInt,
-//    @Kombine(allPossibleUShortParams = [3u, 5u]) val property6: UShort,
-//    val property7: UShort,
-//    val property8: Boolean,
-//    val property9: Int,
-//    @Kombine(allPossibleUShortParams = [3u, 5u]) val property10: Char,
-//)
-
 @Kombine(
-    allPossibleStringParams = ["abc", "cde"]
+    allPossibleStringParams = ["abc", "cde"],
+    allPossibleIntParams = [1],
+    allPossibleFloatParams = [1f, 2f, 3f],
+    allPossibleDoubleParams = [1.0, 2.0],
+    allPossibleLongParams = [1, 2],
+    allPossibleByteParams = [1, 2],
+    allPossibleCharParams = ['1', '2', '3'],
+    allPossibleShortParams = [1, 2],
+    allPossibleUByteParams = [1u, 2u],
+    allPossibleUShortParams = [1u, 2u],
+    allPossibleUIntParams = [1u, 2u],
+    allPossibleULongParams = [1u, 2u, 45u],
 )
 data class MixedSampleDataClass(
-    @Kombine(
-        allPossibleStringParams = ["fgh", "ijk"]
-    ) val property1: String,
+    val property1: Boolean,
+    val property2: Boolean,
+    val property3: String,
+    val property4: Double,
+    val property5: UInt,
+    val property6: UShort,
+    val property7: UShort,
+    val property8: Boolean,
+    val property9: Int,
+    val property10: Char,
+)
+
+@Kombine(
+    allPossibleStringParams = ["abc", "cde"],
+    allPossibleIntParams = [1],
+    allPossibleFloatParams = [1f, 2f, 3f],
+    allPossibleDoubleParams = [1.0, 2.0],
+    allPossibleLongParams = [1, 2],
+    allPossibleByteParams = [1, 2],
+    allPossibleCharParams = ['1', '2', '3'],
+    allPossibleShortParams = [1, 2],
+    allPossibleUByteParams = [1u, 2u],
+    allPossibleUShortParams = [1u, 2u],
+    allPossibleUIntParams = [1u, 2u],
+    allPossibleULongParams = [1u, 2u, 45u],
+)
+data class MixedSampleDataClassSomeParamsAnnotated(
+    val property1: Boolean,
+    @Kombine val property2: Boolean,
+    val property3: String,
+    @Kombine(allPossibleDoubleParams = [5.0, 110.0]) val property4: Double,
+    val property5: UInt,
+    @Kombine(allPossibleUShortParams = [3u, 5u]) val property6: UShort,
+    val property7: UShort,
+    val property8: Boolean,
+    val property9: Int,
+    @Kombine(allPossibleCharParams = ['5']) val property10: Char,
 )
